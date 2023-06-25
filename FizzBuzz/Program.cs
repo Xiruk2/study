7bf5580438cb659a1ddea8d997201bd7e10f6a7c
xiruk <anuvimxiruk@yahoo.co.jp>
2023-06-25 12:37:37 +0900

    [Add] First Commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace FizzBuzzSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            Console.WriteLine(fizzBuzz.GetFizzBuzz());
+            Console.ReadLine();
+        }
+    }
+
+    public class FizzBuzz
+    {
+        private const int FizzNumber = 3;
+        private const int BuzzNumber = 5;
+        private const int FizzBuzzNumber = FizzNumber * BuzzNumber;
+        private const string FizzMessage = "Fizz";
+        private const string BuzzMessage = "Buzz";
+        private const string FizzBuzzMessage = "FizzBuzz";
+
+        private int MaxNumber;
+
+        public FizzBuzz()
+        {
+            var convertInput = 0;
+            
+            if (!int.TryParse(Input(), out convertInput))
+            {
+                throw new FormatException();
+            }
+
+            MaxNumber = convertInput;
+        }
+
+        public FizzBuzz(int maxNumber)
+        {
+            MaxNumber = maxNumber;
+        }
+
+        public string GetFizzBuzz()
+        {
+            string result = string.Empty;
+            for (var i = 1; i < MaxNumber + 1; i++)
+            {
+                result += Excute(i) + " ";
+            }
+            return result.TrimEnd();
+        }
+
+        private string Input()
+        {
+            Console.Write("FizzBuzzMax:");
+            return Console.ReadLine();
+        }
+
+        private string Excute(int target)
+        {
+            if (target % FizzBuzzNumber == 0) return FizzBuzzMessage;
+            if (target % FizzNumber == 0) return FizzMessage;
+            if (target % BuzzNumber == 0) return BuzzMessage;
+            return target.ToString();
+        }
+    }
+}
---
+++ FizzBuzzTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzSample;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz";
+            var fizzBuzz = new FizzBuzz(15);
+            var actual = fizzBuzz.GetFizzBuzz();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
